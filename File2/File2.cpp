b2e42890dd83cbfe94235eae7c1fbef1479bd0c9
ntdsam2830 <ntdsam@gmail.com>
2024-11-26 23:44:26 +0700

    palindrome

--- File2/File2.cpp
+++ File2/File2.cpp
@@ -352,6 +352,21 @@ int flags(vector<int>& A) {
 	return maxFlags;
 }
 
+bool isPalindrome(int x) {
+	if (x < 0) return false;
+
+	long long reversed = 0;
+	long long temp = x;
+
+	while (temp != 0) {
+		int digit = temp % 10;
+		reversed = reversed * 10 + digit;
+		temp /= 10;
+	}
+
+	return (reversed == x);
+}
+
 int main() {
 	vector<int> nums = { 1,5,4,2,9,9,9 };
 	string S = "aabaaaacaabc";
