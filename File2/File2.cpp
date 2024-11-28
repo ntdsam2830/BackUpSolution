6c0422a1b752b2153ebfab8f7fea6b8fa377ceb6
ntdsam2830 <ntdsam@gmail.com>
2024-11-28 15:03:08 +0700

    no AAA BBB

--- File2/File2.cpp
+++ File2/File2.cpp
@@ -367,6 +367,15 @@ bool isPalindrome(int x) {
 	return (reversed == x);
 }
 
+string noThreeConsecutive(int A, int B) {
+	// Implement your solution here
+	if (A == 0) return string(B, 'b');
+	else if (B == 0) return string(A, 'a');
+	else if (A == B) return "ab" + noThreeConsecutive(A - 1, B - 1);
+	else if (A > B) return "aab" + noThreeConsecutive(A - 2, B - 1);
+	else return noThreeConsecutive(A - 1, B - 2) + "abb";
+}
+
 int main() {
 	vector<int> nums = { 1,5,4,2,9,9,9 };
 	string S = "aabaaaacaabc";
@@ -375,7 +384,7 @@ int main() {
 	string t = "nagaram";
 	vector<string> strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
 	vector<vector<int>> grid = { {2, 1, 1},{1, 2, 0},{0, 1, 1} };
-	cout << brackets(a);
+	cout << noThreeConsecutive(1,4);
 	//groupAnagrams(strs);
 	//cout << isValid(a);
 	//vector<int> nums{ 2,4,6,7,11,15 };
